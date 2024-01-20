b31bd7214128da23e7d59baa1d21b1c78a72593e
Nazisg <isgnezrin@gmail.com>
2024-01-20 23:45:58 +0400

    login update

--- src/shared/components/Login/index.tsx
+++ src/shared/components/Login/index.tsx
@@ -1,10 +1,20 @@
 import React from "react";
 import "./style.scss";
 import { Button, Form, Input, Typography } from "antd";
+import { LockOutlined, UserOutlined } from "@ant-design/icons";
 export default function Login() {
+  const onFinish = (values: any) => {
+    console.log("Received values of form: ", values);
+  };
+
   return (
     <div className="login-bg">
-      <Form className="form">
+      <Form
+        className="form"
+        name="normal_login"
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+      >
         <Typography.Title>Login</Typography.Title>
         <Form.Item
           rules={[
@@ -13,10 +23,12 @@ export default function Login() {
               message: "",
             },
           ]}
-          label="User name"
           name={"userName"}
         >
-          <Input placeholder="Enter your user name" />
+          <Input
+            prefix={<UserOutlined className="site-form-item-icon" />}
+            placeholder="Username"
+          />
         </Form.Item>
         <Form.Item
           rules={[
@@ -25,10 +37,13 @@ export default function Login() {
               message: "",
             },
           ]}
-          label="Password"
           name={"password"}
         >
-          <Input.Password placeholder="Enter your user password" />
+          <Input.Password
+            prefix={<LockOutlined className="site-form-item-icon" />}
+            type="password"
+            placeholder="Password"
+          />
         </Form.Item>
         <Button type="primary" htmlType="submit" block>
           Login
