cb2d2bbf6f64d69cd32194bf6e5e9e26b4068378
Nazisg <isgnezrin@gmail.com>
2024-02-05 15:12:37 +0400

    update responsive design

--- src/pages/Employees/index.tsx
+++ src/pages/Employees/index.tsx
@@ -184,7 +184,7 @@ export default function index() {
         size="large"
         className="table"
         pagination={{ pageSize: 10 }}
-        scroll={{ x: "auto" }}
+        scroll={{y:"260px", x: "auto" }}
         columns={columns}
         dataSource={data}
       />
